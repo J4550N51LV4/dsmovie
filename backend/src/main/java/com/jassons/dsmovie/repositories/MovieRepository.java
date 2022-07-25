d102c563d047cfb8cdc8342b34e8668766e89ad4
Jasson Guimaraes <jassonguimaraes@gmail.com>
2022-07-25 13:54:43 -0300

    Find movies

---
+++ backend/src/main/java/com/jassons/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.jassons.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jassons.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository <Movie, Long> {
+	
+}
